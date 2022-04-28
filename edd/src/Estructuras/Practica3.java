55bf5a724dbe7d22340a64fd69a4af9d48beb4c4
monse-arriaga <monse_arriaga@ciencias.unam.mx>
2022-04-28 16:49:02 -0500

    MM

--- edd/src/Estructuras/Practica3.java
+++ edd/src/Estructuras/Practica3.java
@@ -3,6 +3,10 @@ package edd.src.Estructuras;
 import java.util.Iterator;
 
 public class Practica3 {
+
+    static Lista <String> listaCadena = new Lista<>();
+
+
     
     public static void sumaCercana(Lista lista, int N){
 
@@ -16,9 +20,54 @@ public class Practica3 {
 
 
     }
-    public static void permutacionesCadena(String cadena){
 
+    public static String reverseString(char[] s) {
+        int len = s.length;
+    
+    
+        for (int i=0; i < (len/2); i++)
+        {
+            char l = s[i];
+            s[i] = s[len-i-1];
+            s[len-i-1] = l;
+        }
+    
+        String nuevoString = new String(s);
+        return nuevoString;
     }
+
+
+    public static String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+
+    public static void permute(String str, int l, int r)
+    {
+        if (l == r)
+            System.out.println(str);
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+
+    public static void permutacionesCadena(String cadena){
+    int r= cadena.length();
+     permute(cadena, 0, r-1);
+    }
+
+
+
     public static void primosQueSuman(int S, int P, int N){
 
     }
@@ -28,9 +77,11 @@ public class Practica3 {
 
     public static void main(String[] args) {
         //Puedes hacer tus pruebas aqui
-        Lista <Integer> prueba = new Lista<>();
+       // Lista <Integer> prueba = new Lista<>();
+        permutacionesCadena("star");
 
-        sumaCercana(prueba, 20);
+
+
     }
 
 }
