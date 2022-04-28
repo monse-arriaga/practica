6b9ca9f76520e629232e8742bbb111831e7ad5f4
Asahel Main <asahelmain@ciencias.unam.mx>
2022-04-28 16:48:17 -0500

    caca

--- edd/src/Estructuras/Practica3.java
+++ edd/src/Estructuras/Practica3.java
@@ -1,18 +1,81 @@
 package edd.src.Estructuras;
-
 import java.util.Iterator;
+import java.util.Comparator;
 
 public class Practica3 {
     
-    public static void sumaCercana(Lista lista, int N){
+    public static void sumaCercana(Lista<Integer> lista, int n){
+	if(lista.isEmpty()){
+	    System.out.println("La lista es vacía");
+	    return;
+	}
+	
+	if(lista.size() == 1){
+	    System.out.println("La lista solo tiene un elemento");
+	    return;
+	}
+	
+	Lista<Integer> l = lista.mergeSort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+	    });
 
-        IteradorLista<Integer> iterador = lista.iteradorLista();
+	System.out.println(l);
+
+	int c=0,f=0;
+	int contador = 0;
+	Iterator<Integer> iterador1 = l.iterator();
+
+	if(!l.isEmpty()){
+	    iterador1.next();
+	    while(iterador1.hasNext()){
+		iterador1.next();
+		contador++;
+	    }
+	}
+	
+	f = contador;
+	
+	int diferencia = 1000000000;
+	int respuesta1 = 0, respuesta2 = 0;
+	int valorC = 0, valorF = 0;
+
+	Iterator<Integer> iterador2 = l.iterator();
+	IteradorLista<Integer> iterador3 = l.iteradorLista();
+
         
-        int aux= 0;
+	iterador3.end();
+	if(iterador3.hasPrevious()){
+	    valorF = iterador3.previous();
+	}
+	if(iterador2.hasNext()){
+	    valorC = iterador2.next();
+	}
+	
+	while(c<f){
+	    if(Math.abs((valorC + valorF) - n) < diferencia){
+		respuesta1 = valorC;
+		respuesta2 = valorF;
+		diferencia = Math.abs((valorC + valorF) - n);
+	    }
 
-        while(iterador.hasNext()){
-            aux= iterador.next();
-        }
+	    if((valorC + valorF) > n){
+		if(iterador3.hasPrevious()){
+		    valorF = iterador3.previous();
+		    f--;
+		}
+	    }
+	    if((valorC + valorF) <= n){
+	        if(iterador2.hasNext()){
+		    valorC = iterador2.next();
+		    c++;
+		}
+	    }
+	}	
+	
+	System.out.println("Los números con la suma más cercana son: " + respuesta1 + ", " + respuesta2);
 
 
     }
@@ -28,9 +91,24 @@ public class Practica3 {
 
     public static void main(String[] args) {
         //Puedes hacer tus pruebas aqui
-        Lista <Integer> prueba = new Lista<>();
+        Lista <Integer> prueba = new Lista<Integer>();
+	Lista <Integer> prueba2 = new Lista<Integer>();
 
-        sumaCercana(prueba, 20);
+	prueba2.add(4);
+
+	
+	prueba.add(3);
+	prueba.add(1);
+	prueba.add(99);
+	prueba.add(55);
+	prueba.add(20);
+	prueba.add(40);
+	prueba.add(43);
+	prueba.add(15);
+	prueba.add(5);
+	
+        sumaCercana(prueba, 130);
+	sumaCercana(prueba2, 4);
     }
 
 }
